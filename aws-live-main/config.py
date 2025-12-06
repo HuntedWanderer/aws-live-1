b698489cf6a4d018bd2d158c340f41433d177b1a
HuntedWanderer <koaykahwooi@gmail.com>
2025-12-06 15:27:35 +0800

    Rename database from 'employee' to 'employee_db'

--- aws-live-main/config.py
+++ aws-live-main/config.py
@@ -1,7 +1,7 @@
 customhost = "employee.cfiru0kczwhy.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee"
+customdb = "employee_db"
 custombucket = "kahwooi-bucket"
 customregion = "us-east-1"
 
